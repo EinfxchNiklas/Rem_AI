114e1ab99b78653414bc103a73b36b1fa7cda22c
Niklas Ihmels <127397071+EinfxchNiklas@users.noreply.github.com>
2025-02-16 23:10:37 +0100

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-#OPENAI_API_KEY = "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"
-MISTRAL_API_KEY = "vPwXOpx8LzagNCyC9VWLPPbiBMFTxLPk"
-PIPER_PFAD = r"C:\Users\ihmel\Documents\Programmier Projekte\Rem_AI\piper\piper.exe"
-STIMMEN_PFAD = r"C:\Users\ihmel\Documents\Programmier Projekte\Rem_AI\piper\de_DE-eva_k-x_low.onnx"
